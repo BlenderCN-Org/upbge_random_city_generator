c59ef1b4841254833c467cde1aac207270fc2d6e
Joel Gomes da Silva <joelgomes1994@hotmail.com>
2017-11-09 23:33:09 -0200

    Added files of skybox

---
+++ scenery.py
@@ -0,0 +1,24 @@
+import bge, json
+from bge.logic import globalDict
+from bge.render import setMipmapping
+from random import choice, random
+from pprint import pprint
+from ast import literal_eval as litev
+
+def cam_skybox(cont):
+	""" Generates a random city, using a pre made set of rules. """
+	
+	own = cont.owner
+	scene = own.scene
+	active_scenes = bge.logic.getSceneList()
+	
+	# Sensors
+	sensor = cont.sensors[0].positive
+	
+	# Properties
+	
+	#### INITIALIZE ####
+	if sensor:
+		
+		if 'game' in active_scenes:
+			own.worldOrientation = active_scenes['game'].active_camera.worldOrientation
